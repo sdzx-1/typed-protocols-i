c45c1968ae24115acb0ff1cf79cb286b34ce53c2
sdzx-1 <shangdizhixia1993@163.com>
2023-02-06 12:27:49 +0800

    add sCollectDoneF

--- src/Network/Redesign/T1.hs
+++ src/Network/Redesign/T1.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE ImpredicativeTypes #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE QualifiedDo #-}
 {-# LANGUAGE RankNTypes #-}
@@ -40,6 +41,16 @@ data
   C ::
     Message ps st' stNext ->
     C ps '(pl, Tr st' st'' <| que, st) '(pl, Tr stNext st'' <| que, st)
+
+data
+  C1 ps ::
+    (IsPipelined, Queue ps, ps) ->
+    (IsPipelined, Queue ps, ps) ->
+    Type
+  where
+  C1 ::
+    Message ps st' st'' ->
+    C1 ps '(pl, Tr st' st'' <| que, st) '(pl, que, st)
 
 type Peer ::
   forall ps ->
@@ -91,6 +102,14 @@ data Peer ps pr m q k where
     forall ps pr que (st :: ps) (st' :: ps) m q.
     Peer ps pr m q '(Pipelined, que, st') ->
     Peer ps pr m q '(Pipelined, Tr st st <| que, st')
+  SCollectDoneF ::
+    forall ps pr que (st :: ps) (st' :: ps) (st'' :: ps) m q.
+    ( SingI st',
+      ActiveState st'
+    ) =>
+    TheyHaveAgencyProof pr st' ->
+    (C1 ps '(Pipelined, Tr st' st'' <| que, st) I.~> Peer ps pr m q) ->
+    Peer ps pr m q '(Pipelined, Tr st' st'' <| que, st)
 
 instance Functor m => IFunctor (Peer ps pr m) where
   imap f (SReturn q) = SReturn (f q)
@@ -100,6 +119,7 @@ instance Functor m => IFunctor (Peer ps pr m) where
   imap f (SYieldPipelined whap msg pr) = SYieldPipelined whap msg (imap f pr)
   imap f (SCollect thap cont) = SCollect thap (imap f . cont)
   imap f (SCollectDone pr) = SCollectDone (imap f pr)
+  imap f (SCollectDoneF thap cont) = SCollectDoneF thap (imap f . cont)
 
 instance Functor m => IApplicative (Peer ps pr m) where
   ireturn = SReturn
@@ -112,6 +132,7 @@ instance Functor m => IMonad (Peer ps pr m) where
   ibind f (SYieldPipelined whap msg pr) = SYieldPipelined whap msg (ibind f pr)
   ibind f (SCollect thap cont) = SCollect thap (ibind f . cont)
   ibind f (SCollectDone pr) = SCollectDone (ibind f pr)
+  ibind f (SCollectDoneF thap cont) = SCollectDoneF thap (ibind f . cont)
 
 --   SReturn :: q k -> Peer ps pr m q k
 sReturn :: a -> Peer ps pr m (At a i) i
@@ -147,6 +168,11 @@ sCollect = SCollect ReflServerAgency SReturn
 
 sCollectDone :: Peer ps pr m (At () '(Pipelined, que, st')) '(Pipelined, Tr st st <| que, st')
 sCollectDone = SCollectDone (SReturn $ At ())
+
+sCollectDoneF ::
+  (SingI st', ActiveState st', StateAgency st' ~ ServerAgency) =>
+  Peer ps AsClient m (C1 ps '(Pipelined, Tr st' st'' <| que, st)) '(Pipelined, Tr st' st'' <| que, st)
+sCollectDoneF = SCollectDoneF ReflServerAgency SReturn
 
 -- ---------------------------- example
 data PingPong where
@@ -194,14 +220,13 @@ ppClient ::
     '(Pipelined, Empty, StIdle)
 ppClient = I.do
   sYieldPipelined MsgPing
-  sYieldPipelined @_ @'StIdle MsgPing
-  sCollect I.>>= \(C v) -> case v of
-    MsgPong -> I.do
-      sCollectDone
-      sCollect I.>>= \(C v') -> case v' of
-        MsgPong -> I.do
+  sYieldPipelined MsgPing
+  sYieldPipelined @_ @StDone MsgDone
+  sCollectDoneF I.>>= \case
+    C1 MsgPong -> I.do
+      sCollectDoneF I.>>= \case
+        C1 MsgPong -> I.do
           sCollectDone
-          sYield MsgDone
 
 ppClient1 ::
   (Functor m, IMonadFail (Peer PingPong 'AsClient m)) =>
@@ -212,14 +237,11 @@ ppClient1 ::
     (At () '(Pipelined, Empty, StDone))
     '(Pipelined, Empty, StIdle)
 ppClient1 = I.do
-  sYieldPipelined MsgPing
-  sYieldPipelined MsgPing
-  sYieldPipelined MsgPing
+  sYieldPipelined @_ @StIdle MsgPing
+  C1 MsgPong <- sCollectDoneF
+  sYieldPipelined @_ @StIdle MsgPing
+  sYieldPipelined @_ @StIdle MsgPing
+  C1 MsgPong <- sCollectDoneF
+  C1 MsgPong <- sCollectDoneF
   sYieldPipelined @_ @StDone MsgDone
-  C MsgPong <- sCollect
-  sCollectDone
-  C MsgPong <- sCollect
-  sCollectDone
-  C MsgPong <- sCollect
-  sCollectDone
   sCollectDone
